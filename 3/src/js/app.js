efc7c7d3ecaffa9ce0733a74ab6baa0c4292b8d9
Oleg <oleg.kirianov@gmail.com>
2017-03-14 12:39:03 +0300

    task3 v2 finished

--- 3/src/js/app.js
+++ 3/src/js/app.js
@@ -1,10 +1,44 @@
+(function() {
 
-var myApp = new Framework7();
-var $$ = Dom7;
+	var myApp = new Framework7(),
+		$$ = Dom7,
+		fsSelector = 'html';
+		mainView = myApp.addView('.view-main');
 
-var mainView = myApp.addView('.view-main', {});
+	
+	myApp.onPageInit('index plugins', function (page) {
+
+		
+		resetFontSize();
+	
+	  
+	});
+
+	myApp.onPageInit('main2 plugins2', function (page) {
+
+		var viewWidth = $$(window).width();
+		setFontSize(viewWidth);
+	
+	  
+	});
 
 
-myApp.onPageInit('about', function (page) {
-  
-});
+	function setFontSize(viewWidth) {
+	  
+	 
+	  if (viewWidth > 800) { $$(fsSelector).addClass('wide') }
+	  else if (viewWidth > 500) { $$(fsSelector).addClass('standard') }
+	  else { $$(fsSelector).addClass('small') }
+
+	}
+	
+	function resetFontSize() {
+	  
+		  $$(fsSelector).removeClass('wide').removeClass('standard').removeClass('small');
+
+	}
+
+
+	window.$$ = $$;
+
+})();
